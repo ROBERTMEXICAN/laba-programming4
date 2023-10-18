269d9f582f806d6e9b8fab23b316d7b823543653
ROBERTMEXICAN <98842811+ROBERTMEXICAN@users.noreply.github.com>
2023-10-18 20:42:22 +0300

    Add files via upload

---
+++ laba4prog410737/src/Main.java
@@ -0,0 +1,10 @@
+import character.*;
+import items.*;
+import story.Story;
+
+public class Main {
+    public static void main(String[] args) {
+        Story story = new Story();
+        story.go();
+    }
+}
